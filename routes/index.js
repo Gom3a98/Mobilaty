79869e5fc7a69570b5417f6b93419a411c273d6b
Gom3a98 <kkrem551@gmail.com>
2020-04-11 16:18:31 +0200

    Project Setup

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+var user = require("../app/controllers/user/UserController")
+
+var notify = require('../app/controllers/GeneralControllers/notfiy');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/login', user.loginGet);
+router.post('/login', user.loginPost);
+
+router.get('/signup',user.signupGet);
+router.post('/signup',user.signupPost);
+
+router.get('/profile',user.showProfile);
+router.get('/checkUserName',user.checkUserName);
+
+
+router.get('/notifcation',notify.selectAllNotify)
+router.get('/notifcationDetails',notify.selectSpecificNotify)
+
+
+
+module.exports = router;
