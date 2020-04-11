601cc488c610c0a8917b41beaa759af58ef80a60
Gom3a98 <kkrem551@gmail.com>
2020-04-11 18:02:58 +0200

    Login And Sign up

--- app/controllers/user/UserController.js
+++ app/controllers/user/UserController.js
@@ -1,4 +1,4 @@
-var Student = require("../../Model/Student");
+var Student = require("../../model/User");
 
 var formidable = require('formidable');
 //http://localhost:3000/startExam/admin/9/1
@@ -17,37 +17,24 @@ module.exports = {
 		req.session.destroy();
 		res.render('user/userSignUp', { title: 'Sign Up' });
 	},
-	signupPost: (req, res, next) => {
-		var password = Student.hashfun(req.body.password);
-		var name = (req.body.first_name).split("@")[0];
-		var email=req.body.first_name;
-		var phone=req.body.phone_number;
-		Student.createStudent(name,email,password,phone, function (err) {
+	signupPost: (req, res) => {
+		Student.createStudent(req.body, function (err) {
 			if (err) throw err;
 			req.session.userName = req.body.name;
-			res.redirect("/profile");
+			res.send(req.body)
 		});
 	},
 	loginPost: (req, res, next) => {
 		var username = req.body.userName;
 		var x = Student.findStudentByUserName(username, (err, result) => {
-			console.log(result)
 			if (result.length == 1 && Student.compare(req.body.password, result[0].password)) {
-				console.log("amr")
 				req.session.userName = username;
 				req.session.studentId = result[0].id;
-				if(result[0].type != 1){
-					res.send({'success' : true});
-					//res.redirect('/profile');
-				}
-				else {
-					req.session.type = result[0].type;
-					res.send({'success' : false});
-					//res.redirect('/hr/hrDashboard');
-				}
+				res.send(result)
+
 			}
 			else
-				res.redirect('/login')
+			    res.send({'success' : false});
 		});
 	},
 	loginGet: (req, res, next) => {
@@ -68,27 +55,6 @@ module.exports = {
 		else
 			res.redirect('/login')
 		//Authentication(req,res).then(res.send('Welcome back, ' + req.session.userName + '!'));
-	},
-
-	uploadCV:async function (req, res, next) {
-		console.log(req.body);
-		if (await Authentication(req, res)) {
-			var form = new formidable.IncomingForm();
-			form.parse(req, function (err, fields, files) {
-				var posId = fields.posId;
-				var title = fields.title;
-				var oldpath = files.filetoupload.path;
-				var newpath = 'F:/Level 4 Term 1/IA Project/OnlineTestV3/public/CVS/' + req.session.userName +"_"+title+".pdf";
-				var absPath = req.session.userName +"_"+title.trim()+".pdf";
-				fs.removeSync(newpath, null);
-				console.log(fields.posId)
-				fs.move(oldpath, newpath, function (err) {
-					if (err) throw err;
-					 	Applicant.saveApplication(req.session.userName , posId , absPath ,null);
-				});
-				res.redirect("/profile");
-			})
-		}
 	},
 
 	checkUserName:function(req,res,next){
