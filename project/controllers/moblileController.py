e5e2d3d7620c245a3c72128a5d91d3cdf259fa23
amrfahmy5 <elkingamr46@yahoo.com>
2020-05-28 18:31:56 +0200

    update dataBase

--- project/controllers/moblileController.py
+++ project/controllers/moblileController.py
@@ -1,30 +1,3 @@
-'''
-import numpy as np
-import pandas as pd
-
-data = pd.read_csv('train.csv')
-x = data.iloc[:, :-1].values
-y = data.iloc[:, -1].values
-m, n = len(x), len(x[0])+1
-x = np.append(arr = np.ones((m, 1)).astype(int), values = x, axis = 1)
-
-from sklearn.model_selection import train_test_split
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
-
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
-from sklearn.externals import joblib
-
-pipe = make_pipeline(StandardScaler(), LogisticRegression())
-pipe.fit(x_train, y_train)
-y_pred = pipe.predict(x_train)
-
-print(pipe.score(x_train, y_train))#0.975625
-print(pipe.score(x_test, y_test)) #0.965
-
-joblib.dump(pipe, 'predictPrice.pkl')
-'''
 from __future__ import print_function
 from project import app
 from flask import render_template, redirect, url_for, jsonify, request
